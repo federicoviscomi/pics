4044e74978313816fec811ecdb113d7f1d2f93ae
Federico Viscomi <federico.viscomi@dh.com>
2019-02-05 16:43:03 +0000

    7.76.start

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+                Search Bar!
+            </div>
+        );
+    }
+};
+export default SearchBar;
