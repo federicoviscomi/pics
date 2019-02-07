ac52c5e5eeecbc9054474c7c8b67cfd7535e97ce
federicoviscomi <f@neterea.com>
2019-02-07 21:09:50 +0000

    7.81.start

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import SearchBar from "./SearchBar";
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container" style={{marginTop: '10px'}}>
             <SearchBar/>
         </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,25 @@ import React from 'react';
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {term: undefined};
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({term: event.target.value});
     }
 
     render() {
         return (
-            <div>
-                Search Bar!
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Image search</label>
+                        <input type="text"
+                               value={this.state.term}
+                               onChange={event => this.setState({term: event.target.value})}/>
+                    </div>
+                </form>
             </div>
         );
     }
